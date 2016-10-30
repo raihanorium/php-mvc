9664a6c0bce4e2356f6312801afc5c7e3a685f81
Raihan <raihan.dk@gmail.com>
2016-10-30 22:08:14 +0600

    Error checking if view exists, and 404 page

---
+++ app/views/404.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>404: Not Found!</title>
+</head>
+
+<body>
+Oops... Cannot find the page!
+</body>
+</html>
--- index.php
+++ index.php
@@ -16,8 +16,26 @@ if(isset($vars['p'])){
 }
 
 $controllerFileName = ucfirst($controller) . 'Controller';
-require_once('app/controllers/' . $controllerFileName . '.php');
-$controllerClassPath = 'app\\controllers\\' . $controllerFileName;
-$controller = new $controllerClassPath;
-$controller->$action($request);
-?>
+$controllerFilePath = 'app/controllers/' . $controllerFileName . '.php';
+if(file_exists($controllerFilePath)) {
+    require_once($controllerFilePath);
+    $controllerClassPath = 'app\\controllers\\' . $controllerFileName;
+    $controller = new $controllerClassPath;
+
+    if(method_exists($controller, $action)) {
+        $controller->$action($request);
+    } else{
+        render404();
+    }
+} else{
+    render404();
+}
+
+function render404(){
+    $filePath = 'app/views/404.php';
+    if(file_exists($filePath)){
+        require_once $filePath;
+    } else{
+        echo '<h1>404: Not Found!</h1>';
+    }
+}
