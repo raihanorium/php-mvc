65eb1f2d53f963029ddac2603ca13e6aa8818bc6
raihanorium <raihan.dk@gmail.com>
2016-11-06 14:01:26 +0600

    User model

--- app/model/User.php
+++ app/model/User.php
@@ -1,10 +1,12 @@
 <?php
 
-class User{
-	$id;
-	$full_name;
-	$username;
-	$email;
-	$password;
-	$roles = array('user');
+namespace model;
+
+class User {
+    public $id;
+    public $full_name;
+    public $username;
+    public $email;
+    public $password;
+    public $roles = array('user');
 }
