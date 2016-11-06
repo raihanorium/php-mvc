13d05a6d45ce1be78ee0e145c1ba9fb16b2ca352
raihanorium <raihan.dk@gmail.com>
2016-11-06 14:50:59 +0600

    Reseller model

---
+++ app/model/Reseller.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ataul.raihan
+ * Date: 11/6/2016
+ * Time: 2:34 PM
+ */
+
+namespace model;
+
+
+class Reseller {
+    public $id;
+    public $full_name;
+    public $username;
+    public $email;
+    public $password;
+    public $is_active;
+    public $created_at;
+}
