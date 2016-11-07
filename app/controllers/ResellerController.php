6a47c78d4f2f1f526883b2c83eaac947b3a49db1
Raihan <raihan.dk@gmail.com>
2016-11-07 22:30:43 +0600

    Singleton reseller service

--- app/controllers/ResellerController.php
+++ app/controllers/ResellerController.php
@@ -1,25 +1,24 @@
 <?php
 namespace app\controllers;
 
+require_once 'app/service/ResellerService.php';
+
 use core\Controller;
+use services\ResellerService;
 
 /**
  * @hasAnyRole(admin)
  */
 class ResellerController extends Controller {
+    private $resellerService;
+
+    public function __construct() {
+        $this->resellerService = ResellerService::Instance();
+        parent::__construct();
+    }
+
     public function index($request) {
-        $resellers = array(
-            array('id' => 1, 'full_name' => 'Mr Reseller 1', 'username' => 'reseller1', 'email' => 'reseller1@email.com', 'password' => 'password', 'is_active' => true, 'created_at' => '2016-11-06'),
-            array('id' => 2, 'full_name' => 'Mr Reseller 2', 'username' => 'reseller2', 'email' => 'reseller2@email.com', 'password' => 'password', 'is_active' => true, 'created_at' => '2016-11-06'),
-            array('id' => 3, 'full_name' => 'Mr Reseller 3', 'username' => 'reseller3', 'email' => 'reseller3@email.com', 'password' => 'password', 'is_active' => true, 'created_at' => '2016-11-06'),
-            array('id' => 4, 'full_name' => 'Mr Reseller 4', 'username' => 'reseller4', 'email' => 'reseller4@email.com', 'password' => 'password', 'is_active' => true, 'created_at' => '2016-11-06'),
-            array('id' => 5, 'full_name' => 'Mr Reseller 5', 'username' => 'reseller5', 'email' => 'reseller5@email.com', 'password' => 'password', 'is_active' => true, 'created_at' => '2016-11-06'),
-            array('id' => 6, 'full_name' => 'Mr Reseller 6', 'username' => 'reseller6', 'email' => 'reseller6@email.com', 'password' => 'password', 'is_active' => true, 'created_at' => '2016-11-06'),
-            array('id' => 7, 'full_name' => 'Mr Reseller 7', 'username' => 'reseller7', 'email' => 'reseller7@email.com', 'password' => 'password', 'is_active' => true, 'created_at' => '2016-11-06'),
-            array('id' => 8, 'full_name' => 'Mr Reseller 8', 'username' => 'reseller8', 'email' => 'reseller8@email.com', 'password' => 'password', 'is_active' => true, 'created_at' => '2016-11-06'),
-            array('id' => 9, 'full_name' => 'Mr Reseller 9', 'username' => 'reseller9', 'email' => 'reseller9@email.com', 'password' => 'password', 'is_active' => true, 'created_at' => '2016-11-06'),
-            array('id' => 10, 'full_name' => 'Mr Reseller 10', 'username' => 'reseller10', 'email' => 'reseller10@email.com', 'password' => 'password', 'is_active' => true, 'created_at' => '2016-11-06')
-        );
+        $resellers = $this->resellerService->getAll();
         $this->view->renderTemplate($resellers);
     }
 
---
+++ app/service/ResellerService.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Raihan
+ * Date: 11/7/2016
+ * Time: 9:56 PM
+ */
+
+namespace services;
+
+require_once 'Database.php';
+
+final class ResellerService {
+    private $db;
+
+    private function __construct(){
+        $this->db = Database::Instance();
+    }
+
+    public static function Instance(){
+        static $inst = null;
+        if ($inst === null) {
+            $inst = new ResellerService();
+        }
+        return $inst;
+    }
+
+    public function getAll(){
+        $resellers = array(
+            array('id' => 1, 'full_name' => 'Mr Reseller 1', 'username' => 'reseller1', 'email' => 'reseller1@email.com', 'password' => 'password', 'is_active' => true, 'created_at' => '2016-11-06'),
+            array('id' => 2, 'full_name' => 'Mr Reseller 2', 'username' => 'reseller2', 'email' => 'reseller2@email.com', 'password' => 'password', 'is_active' => true, 'created_at' => '2016-11-06'),
+            array('id' => 3, 'full_name' => 'Mr Reseller 3', 'username' => 'reseller3', 'email' => 'reseller3@email.com', 'password' => 'password', 'is_active' => true, 'created_at' => '2016-11-06'),
+            array('id' => 4, 'full_name' => 'Mr Reseller 4', 'username' => 'reseller4', 'email' => 'reseller4@email.com', 'password' => 'password', 'is_active' => true, 'created_at' => '2016-11-06'),
+            array('id' => 5, 'full_name' => 'Mr Reseller 5', 'username' => 'reseller5', 'email' => 'reseller5@email.com', 'password' => 'password', 'is_active' => true, 'created_at' => '2016-11-06'),
+            array('id' => 6, 'full_name' => 'Mr Reseller 6', 'username' => 'reseller6', 'email' => 'reseller6@email.com', 'password' => 'password', 'is_active' => true, 'created_at' => '2016-11-06'),
+            array('id' => 7, 'full_name' => 'Mr Reseller 7', 'username' => 'reseller7', 'email' => 'reseller7@email.com', 'password' => 'password', 'is_active' => true, 'created_at' => '2016-11-06'),
+            array('id' => 8, 'full_name' => 'Mr Reseller 8', 'username' => 'reseller8', 'email' => 'reseller8@email.com', 'password' => 'password', 'is_active' => true, 'created_at' => '2016-11-06'),
+            array('id' => 9, 'full_name' => 'Mr Reseller 9', 'username' => 'reseller9', 'email' => 'reseller9@email.com', 'password' => 'password', 'is_active' => true, 'created_at' => '2016-11-06'),
+            array('id' => 10, 'full_name' => 'Mr Reseller 10', 'username' => 'reseller10', 'email' => 'reseller10@email.com', 'password' => 'password', 'is_active' => true, 'created_at' => '2016-11-06')
+        );
+        return $resellers;
+    }
+}
