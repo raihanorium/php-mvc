a6724cd3a5ea4361cd5c912a5ef0c48180b1b3d0
Raihan <raihan.dk@gmail.com>
2016-11-07 23:00:50 +0600

    Application wide constants

---
+++ core/ApplicationConstants.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Raihan
+ * Date: 11/7/2016
+ * Time: 10:43 PM
+ */
+
+namespace core;
+
+
+interface ApplicationConstants {
+    const CREATE_RESELLER_TABLE = "
+        CREATE TABLE IF NOT EXISTS `reseller` (
+          `id` INT NOT NULL AUTO_INCREMENT ,
+          `full_name` VARCHAR(255) NOT NULL ,
+          `username` VARCHAR(255) NOT NULL ,
+          `email` VARCHAR(255) NOT NULL ,
+          `password` VARCHAR(64) NOT NULL ,
+          `is_active` BOOLEAN NOT NULL DEFAULT FALSE ,
+          `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+          PRIMARY KEY (`id`),
+          UNIQUE `username_unique` (`username`),
+          UNIQUE `email_unique` (`email`)) ENGINE = InnoDB;
+        ";
+    const GET_ALL_RESELLERS = "SELECT * FROM `reseller`";
+}
