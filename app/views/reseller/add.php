00c5cf58e5102a4e406c5ba693c3b43c1ec190e2
Raihan <raihan.dk@gmail.com>
2016-11-07 21:55:03 +0600

    Add reseller form

--- app/views/reseller/add.php
+++ app/views/reseller/add.php
@@ -5,33 +5,54 @@
         <div class="panel-heading">Add Reseller</div>
         <div class="panel-body">
             <form action="?p=reseller&a=save" method="post">
-                <div class="form-group">
-                    <label for="fullName">Full Name</label>
-                    <input type="text" class="form-control" id="fullName" name="full_name" placeholder="Full Name" />
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        <label for="fullName">Full Name</label>
+                        <input type="text" class="form-control" id="fullName" name="full_name" placeholder="Full Name" />
+                    </div>
+
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" class="form-control" id="username" name="username" placeholder="Username" />
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Email address</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Email" />
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Password" />
+                    </div>
+
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" id="isActive" name="is_active" /> Active
+                        </label>
+                    </div>
                 </div>
 
-                <div class="form-group">
-                    <label for="username">Username</label>
-                    <input type="text" class="form-control" id="username" name="username" placeholder="Username" />
+                <div class="col-sm-6">
+                    <fieldset>
+                        <legend>Services</legend>
+                        <div class="checkbox">
+                            <label><input type="checkbox" value="flexiload" />FlexiLoad</label>
+                        </div>
+                        <div class="checkbox">
+                            <label><input type="checkbox" value="bkash" />bKash</label>
+                        </div>
+                        <div class="checkbox">
+                            <label><input type="checkbox" value="rocket" />Rocket</label>
+                        </div>
+                    </fieldset>
                 </div>
 
-                <div class="form-group">
-                    <label for="email">Email address</label>
-                    <input type="email" class="form-control" id="email" name="email" placeholder="Email" />
-                </div>
+                <div class="clearfix"></div>
 
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" class="form-control" id="password" name="password" placeholder="Password" />
+                <div class="col-sm-12">
+                    <button type="submit" class="btn btn-primary pull-right">Add</button>
                 </div>
-
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox" id="isActive" name="is_active" /> Active
-                    </label>
-                </div>
-
-                <button type="submit" class="btn btn-primary pull-right">Add</button>
             </form>
         </div>
     </div>
