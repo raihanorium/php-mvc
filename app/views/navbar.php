93f4444f9346d2a9014b516906e597980ef4e099
raihanorium <raihan.dk@gmail.com>
2016-11-20 17:30:23 +0600

    Navbar permission

--- app/views/navbar.php
+++ app/views/navbar.php
@@ -15,7 +15,9 @@
 		<div class="collapse navbar-collapse navbar-ex1-collapse">
 			<ul class="nav navbar-nav">
 				<li <?php if (isset($_GET['p'])) { echo ($_GET['p'] == 'home') ? 'class="active"' : ''; } else { echo 'class="active"'; }?>><a href=".">Home</a></li>
-                <li class="dropdown <?php if (isset($_GET['p'])) {echo ($_GET['p'] == 'reseller') ? 'active' : '';} ?>">
+
+                <?php if ($_SESSION['LOGGED_IN_USER']['role'] == 1): ?>
+				<li class="dropdown <?php if (isset($_GET['p'])) {echo ($_GET['p'] == 'reseller') ? 'active' : '';} ?>">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Resellers <b class="caret"></b></a>
                     <ul class="dropdown-menu">
 						<li><a href="?p=reseller">List All</a></li>
@@ -23,6 +25,8 @@
                     </ul>
                 </li>
 				<li class="<?php if (isset($_GET['p'])) {echo ($_GET['p'] == 'service') ? 'active' : '';} ?>"><a href="?p=service">Services</a></li>
+                <?php endif; ?>
+
 				<li class="<?php if (isset($_GET['p'])) {echo ($_GET['p'] == 'transaction') ? 'active' : '';} ?>"><a href="?p=transaction">Transactions</a></li>
 			</ul>
 
